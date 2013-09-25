8a3e3318c6d1861a13bb867e3306c9ebb3246ed9
Mark DenHoed <mcdenhoed@gmail.com>
2013-09-25 16:39:32 -0500

    Generated stubs for interfaces and stuff.

---
+++ Project2/RReceiveUDP.java
@@ -0,0 +1,66 @@
+
+public class RReceiveUDP implements edu.utulsa.unet.RReceiveUDPI{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getFilename() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLocalPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getModeParameter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean receiveFile() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setFilename(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean setLocalPort(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setMode(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setModeParameter(long arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Project2/RSendUDP.java
@@ -0,0 +1,92 @@
+import java.net.InetSocketAddress;
+
+
+public class RSendUDP implements edu.utulsa.unet.RSendUDPI{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getFilename() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLocalPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getModeParameter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public InetSocketAddress getReceiver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getTimeout() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean sendFile() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setFilename(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean setLocalPort(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setMode(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setModeParameter(long arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setReceiver(InetSocketAddress arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setTimeout(long arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
