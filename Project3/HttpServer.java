e64523c682517355a7a5a098a6b996fefba341a6
Mark <mark-denhoed@utulsa.edu>
2013-12-15 21:23:49 -0600

    Stupid windows.

--- Project3/HttpServer.java
+++ Project3/HttpServer.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class HttpServer {
 
@@ -19,8 +18,8 @@ public class HttpServer {
 		}
 		
 		private boolean containsHostline(ArrayList<String> request){
-			for(Iterator<String> i = request.iterator(); i.hasNext(); )
-				if(i.next().split(" ")[0] == "Host:")
+			for(int i = 0; i < request.size() ; i++)
+				if(request.get(i).split(" ")[0].equals("Host:"))
 					return true;
 			return false;
 		}
@@ -31,6 +30,35 @@ public class HttpServer {
 				badRequest(o);
 				return;
 			}
+			
+			String URI = request.get(0).split(" ")[1];
+			File f = new File("public_html" + URI);
+			if(f.exists() && !f.isDirectory()){
+				FileInputStream reader = new FileInputStream("public_html" + URI);
+				byte[] fileData = new byte[(int)f.length()];
+				reader.read(fileData);
+				
+				String type = "";
+				if(URI.endsWith(".htm") || URI.endsWith("html"))
+					type = "text/html";
+				else if(URI.endsWith(".gif"))
+					type = "image/gif";
+				else if(URI.endsWith(".jpg") || URI.endsWith("jpeg"))
+					type = "image/jpeg";
+				else if(URI.endsWith(".pdf"))
+					type = "application/pdf";
+				
+				String response = "HTTP/1.1 200 \r\n";
+				response += entity((int)f.length(), type);
+				response += "\r\n";
+				
+				char[] yolo = new char[response.length() + fileData.length];
+				System.arraycopy(response.toCharArray(), 0, yolo,  0, response.length());
+				System.arraycopy(new String(fileData).toCharArray(), 0, yolo, response.length(), fileData.length);
+				o.write(yolo);
+				o.flush();
+				System.out.println(yolo);
+			}else notFound(o);
 		}
 
 		private void handleHEAD(OutputStreamWriter o, ArrayList<String> request) throws IOException{
@@ -38,6 +66,28 @@ public class HttpServer {
 				badRequest(o);
 				return;
 			}
+			String URI = request.get(0).split(" ")[1];
+			File f = new File("public_html" + URI);
+			if(f.exists() && !f.isDirectory()){
+			
+				String type = "";
+				if(URI.endsWith(".htm") || URI.endsWith("html"))
+					type = "text/html";
+				else if(URI.endsWith(".gif"))
+					type = "image/gif";
+				else if(URI.endsWith(".jpg") || URI.endsWith("jpeg"))
+					type = "image/jpeg";
+				else if(URI.endsWith(".pdf"))
+					type = "application/pdf";
+				
+				String response = "HTTP/1.1 200 \r\n";
+				response += entity((int)f.length(), type);
+				response += "\r\n";
+				
+				o.write(response);
+				o.flush();
+				System.out.println(response);
+			}else notFound(o);
 		}
 		
 		private void badRequest(OutputStreamWriter o) throws IOException{
@@ -46,14 +96,23 @@ public class HttpServer {
 		
 		private void notFound(OutputStreamWriter o) throws IOException{
 			
+			String response = "HTTP/1.1 404 File not found.\r\n";
+			response += entity(0, "");
+			System.out.println(response);
+			o.write(response);
+			o.flush();
 		}
 		
+		private String entity(int length, String type){
+			return "Server: yoloswag/0.01\r\nContent-Length: " + length + "\r\nContent-Type: " + type + "\r\n";
+		}
 		private void notImplemented(OutputStreamWriter o) throws IOException{
 			String response = "HTTP/1.1 501 This functionality is not implemented yet. \r\n";
 			response += "Server: yoloswag/0.01\r\n";
 			response += "Content-Length: 0\r\n";
 			response += "Content-Type: 0";
 			o.write(response);
+			o.flush();
 		}
 		public void run(){
 			try {
@@ -61,25 +120,29 @@ public class HttpServer {
 				OutputStreamWriter o = new OutputStreamWriter(socket.getOutputStream());
 				ArrayList<String> request = new ArrayList<String>();
 				String temp = r.readLine();
-				while(temp != null){
+				while(temp != null && !temp.equals("")){
 					request.add(temp);
-					temp = r.readLine();
+					if(r.ready())
+						temp = r.readLine();
 				}
-				String[] method = request.get(0).split(" ");
-				try{
-					switch(method[0]){
-					case "GET":
-						handleGET(o, request);
-						break;
-					case "HEAD":
-						handleHEAD(o, request);
-						break;
-					default:
-						notImplemented(o);
+				if(request.size() > 0){
+					String[] method = request.get(0).split(" ");
+					try{
+						switch(method[0]){
+						case "GET":
+							handleGET(o, request);
+							break;
+						case "HEAD":
+							handleHEAD(o, request);
+							break;
+						default:
+							notImplemented(o);
+						}
+					}catch(IOException e){
+						System.out.println("Somthing broke.");
 					}
-				}catch(IOException e){
-					System.out.println("Somthing broke.");
-				}
+				}else
+					badRequest(o);
 			} catch (IOException e) {
 				// TODO Auto-generated catch blocks
 				e.printStackTrace();
